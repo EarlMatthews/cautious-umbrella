f7cdb9057c6e7f5f4ea449f83229f167d1a12ad4
Earl Matthews <EarlMatthews@users.noreply.github.com>
2022-06-21 12:14:45 +0000

    first commit

---
+++ tree-levels.py
@@ -0,0 +1,69 @@
+from collections import deque
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+# def tree_levels(root):
+#   if root is None:
+#     return []
+#   stack  = [(root, 0)]  # stack will hold node value and level
+#   levels = []  # tree_levels return value
+
+#   while stack:
+#     node, level_num = stack.pop() # (node_value, level)
+
+#     if len(levels) == level_num:
+#       levels.append([node.val])
+#     else:
+#       levels[level_num].append(node.val)
+
+#     if node.right is not None:
+#       stack.append((node.right, level_num + 1))
+#     if node.left is not None:
+#       stack.append((node.left, level_num + 1))
+#   return levels
+
+
+def tree_levels(root):
+    '''
+    '''
+    if root is None:
+        return []
+    queue = deque([(root, 0)])  # queue will hold node value and level
+    levels = []  # tree_levels return value
+
+    while queue:
+        node, level_num = queue.popleft()  # (node_value, level)
+
+        if len(levels) == level_num:
+            levels.append([node.val])
+        else:
+            levels[level_num].append(node.val)
+
+        if node.left is not None:
+            queue.append((node.left, level_num + 1))
+
+        if node.right is not None:
+            queue.append((node.right, level_num + 1))
+
+    return levels
+ 
+print(tree_levels(a))
