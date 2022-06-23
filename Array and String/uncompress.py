e588835304062d26681086781d19e0d6be11d848
Earl Matthews <EarlMatthews@users.noreply.github.com>
2022-06-23 08:17:55 +0000

    added test cases

--- Array and String/uncompress.py
+++ Array and String/uncompress.py
@@ -8,3 +8,19 @@ def uncompress(s):
 			result += num * char
 			i = j+1
 	return result
+
+
+# Test 00
+print(uncompress("2c3a1t")) # -> 'ccaaat'
+
+# Test 01
+print(uncompress("4s2b")) # -> 'ssssbb'
+
+# Test 02
+print(uncompress("2p1o5p"))
+
+# Test 03
+print(uncompress("3n12e2z"))
+
+# Test 04
+print(uncompress("127y"))
