2a9ec1854b845db53f5d411797953f4c765c8f2d
Earl Matthews <EarlMatthews@users.noreply.github.com>
2022-06-25 10:22:42 +0000

    add desc

--- Array and String/five-sort.py
+++ Array and String/five-sort.py
@@ -1,4 +1,10 @@
 def five_sort(nums):
+    '''
+    Write a function, five_sort, that takes in a list of numbers as an argument. 
+    The function should rearrange elements of the list such that all 5s appear at the end. 
+    Your function should perform this operation in-place by mutating the original list. The function should return the list.
+    Elements that are not 5 can appear in any order in the output, as long as all 5s are at the end of the list.
+    '''
     head = 0 # start front at index 0
     tail = len(nums) - 1 # start tail at end of list
     while head < tail:
